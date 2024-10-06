6355cbe93c91c7029c271b8b04902c88ec1bfbcc
145a <133004095+54145a@users.noreply.github.com>
2024-10-06 10:59:36 +0800

    http

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-//import console from "node:console";
-console.log("hello, world");
+import http from 'node:http';
+
+http.createServer((request, response) => {
+    /*let url = new URL(request.url);
+    if (url.pathname === "/robots.txt") {
+        return new Response("User-Agent: *\nDisallow: /");
+    }
+    url.hostname = targetHostname;
+    let newRequest = new Request(url, request);
+    /*newRequest.headers.set("Access-Control-Allow-Origin", "*");
+                    newRequest.headers.delete("Origin");
+                                            newRequest.headers.delete("Referrer");*/
+    response.write(JSON.stringify(request));
+    //return fetch(newRequest);
+}).listen();
+let targetHostname = "zh.wikipedia.org";
