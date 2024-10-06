262f3c5c1e117ac9d8b2905b721c6a03053db79d
145a <133004095+54145a@users.noreply.github.com>
2024-10-06 15:09:26 +0800

    bbb

--- worker.js
+++ worker.js
@@ -27,12 +27,15 @@ export default {
         newRequest.headers.keys().filter(v => v.startsWith("cf")).forEach(v => newRequest.headers.delete(v));
         let response = await fetch(newRequest);
         let location = response.headers.get("Location");
+        console.log(location, 0)
         if (location) {
             response.headers.set("Location", location.replace(targetHostname, proxyHostname));
         }
+        console.log(location, 1);
+        let newText = (await response.text()).replace(targetHostname, proxyHostname);
         /*let r = {};
         response.headers.forEach((v, k) => r[k] = v);
         return new Response(JSON.stringify(r, void 0, 4));*/
-        return response;
+        return new Response(newText, response);
     }
 };
