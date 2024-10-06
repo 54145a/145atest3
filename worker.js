d0b7e92f6e8ec0d8cdc6a5359071af4fe5890060
145a <133004095+54145a@users.noreply.github.com>
2024-10-06 15:33:29 +0800

    ohhhhh

--- worker.js
+++ worker.js
@@ -16,26 +16,27 @@ export default {
             return new Response("User-Agent: *\nDisallow: /");
         }
         let newRequest = new Request(request.url.replace(proxyHostname, targetHostname), {
-            headers: request.headers,
+            headers: new Headers(),
             body: request.body
         });
+        request.headers.forEach((v, k) => newRequest.headers.set(k, v.replace(proxyHostname, targetHostname)));
         newRequest.headers.set("Access-Control-Allow-Origin", "*");
         newRequest.headers.delete("Origin");
         newRequest.headers.delete("Referrer");
         newRequest.headers.delete("Host");
         newRequest.headers.keys().filter(v => v.startsWith("cf")).forEach(v => newRequest.headers.delete(v));
-        newRequest.headers.forEach((v, k) => response.headers.set(k, v.replace(proxyHostname, targetHostname)));
         let response = await fetch(newRequest);
+        let newText = (await response.text()).replace(targetHostname, proxyHostname);
+        let newResponse = new Response(newText);
         /*let location = response.headers.get("Location");
         if (location) {
             console.log("location",location);
             response.headers.set("Location", location.replace(targetHostname, proxyHostname));
         }*/
-        response.headers.forEach((v, k) => response.headers.set(k, v.replace(targetHostname, proxyHostname)));
-        let newText = (await response.text()).replace(targetHostname, proxyHostname);
+        response.headers.forEach((v, k) => newResponse.headers.set(k, v.replace(targetHostname, proxyHostname)));
         /*let r = {};
         response.headers.forEach((v, k) => r[k] = v);
         return new Response(JSON.stringify(r, void 0, 4));*/
-        return new Response(newText, response);
+        return newResponse;
     }
 };
